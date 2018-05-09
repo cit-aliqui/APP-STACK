96969588340b8441062a3d388c1bf4ab2c5c3b26
Raghu K <raghuk.vit@gmail.com>
2018-05-09 02:58:23 +0000

    Updating APP-STACK

--- Stack-3/python-api-test.py
+++ Stack-3/python-api-test.py
@@ -5,30 +5,39 @@ import json
 from StringIO import StringIO
 import sys
 
+import urllib2
+try: 
+    response = urllib2.urlopen('http://10.142.0.2:8080/student-api/')
+except urllib2.HTTPError, e:
+    print 'Test :: Connection :: FAILURE'
+    sys.exit()
+except urllib2.URLError, e:
+    print 'Test :: Connection :: FAILURE'
+    sys.exit()
+print 'Test :: Connection :: SUCCESS'
+
 ### Only to test List values
 buffer = StringIO()
 c = pycurl.Curl()
-c.setopt(c.URL, 'http://35.231.156.222:8080/student-api/student/list')
+c.setopt(c.URL, 'http://10.142.0.2:8080/student-api/student/list')
 c.setopt(c.WRITEFUNCTION, buffer.write)
 c.perform()
 c.close()
 body = buffer.getvalue()
 stat = json.loads(body)
 stat_list = stat['httpStatus']
-
-
 if stat_list >= 400 and stat_list < 500:
     message = stat['data']['message']
-    print 'Test :: GET Students :: FAILURE'
+    print 'Test :: LIST Students :: FAILURE'
     print '\tError -> ' + message
     #sys.exit(1)
 else:
-    print 'Test :: GET Students :: SUCCESS'
+    print 'Test :: LIST Students :: SUCCESS'
 
 
 ### Only to PUT values.
 
-apiURL = 'http://35.231.156.222:8080/student-api/student/'
+apiURL = 'http://10.142.0.2:8080/student-api/student/'
 c = pycurl.Curl()
 c.setopt(c.URL, apiURL)
 c.setopt(pycurl.HTTPHEADER,['Content-Type: application/json'])
@@ -36,17 +45,28 @@ c.setopt(c.POSTFIELDS, '{ "studentName": "Meghan Mahadev", "studentAddr": "Hyder
 c.setopt(c.WRITEFUNCTION, buffer.write)
 c.perform()
 c.close()
-body = buffer.getvalue().replace('}}','}}|').split('|')
-print body
-#stat = json.loads(body)
-sys.exit()
+body = buffer.getvalue().replace('}}','}}|').split('|')[0]
+## Adding this code for delete purpose
+output = buffer.getvalue().replace('}}','}}|').split('|')
+cc = len(output)
+out1 = output[cc-2]+'}'
+stat = json.loads(out1)
+stat_id = stat['data']['object']['student_id']
+##########
+
+
+try:
+    stat = json.loads(body)
+except TypeError:
+    body = buffer.getvalue().replace('}}','}}|').split('|')[1]
+    stat = json.loads(body)
 stat_put = stat['httpStatus']
 if stat_put >= 200 and stat_put < 300:
-    print 'Test :: PUT Students :: SUCCESS'
+    print 'Test :: ADD Student :: SUCCESS'
 else:
     stat = json.loads(body[1])
     message = stat['data']['message']
-    print 'Test :: PUT Students :: FAILURE'
+    print 'Test :: ADD Student :: FAILURE'
     print '\tError -> ' + message
     sys.exit(1)
 
